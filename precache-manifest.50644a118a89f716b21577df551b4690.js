a161e2a9fc144ad8742140129d534a302869d119
test <test@test.com>
2019-11-19 16:44:41 +0800

    6

---
+++ precache-manifest.50644a118a89f716b21577df551b4690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f40beb92ac954e017d428dee829c1bd",
+    "url": "timg.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "f7161f8be9b82ed7ca8f",
+    "url": "js/chunk-vendors.4dcbfa82.js"
+  },
+  {
+    "revision": "f6d9265f8bf51b86b95e",
+    "url": "js/app.43e7c660.js"
+  },
+  {
+    "revision": "03a7ea8a6c3fc968ec497e5cc25e187c",
+    "url": "index.html"
+  },
+  {
+    "revision": "3f40beb92ac954e017d428dee829c1bd",
+    "url": "img/timg.3f40beb9.jpg"
+  },
+  {
+    "revision": "f6d9265f8bf51b86b95e",
+    "url": "css/app.944ad219.css"
+  }
+];
