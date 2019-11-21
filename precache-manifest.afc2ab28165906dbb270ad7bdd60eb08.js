07bae9bb5767615eb9a21c3f223b6e6e6ad7ad88
test <test@test.com>
2019-11-21 10:52:01 +0800

    新增

---
+++ precache-manifest.afc2ab28165906dbb270ad7bdd60eb08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc60fafd6ffc7390f95b",
+    "url": "css/app.ef6096b8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "223349a3fabb5a91de8aef56ce5d0bf6",
+    "url": "index.html"
+  },
+  {
+    "revision": "47fa38af6557ef652915",
+    "url": "js/about.146d95dc.js"
+  },
+  {
+    "revision": "bc60fafd6ffc7390f95b",
+    "url": "js/app.835acc5a.js"
+  },
+  {
+    "revision": "c3207514bfa004464459",
+    "url": "js/chunk-vendors.366cff6e.js"
+  },
+  {
+    "revision": "134630b86942c9e2cb2ce27ba162fe83",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
